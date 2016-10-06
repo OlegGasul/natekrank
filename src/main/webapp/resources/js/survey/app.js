f1435ed8ba85472adaa239dfa1b0642b320b909c
oleg <oleg.gasul@gmail.com>
2016-10-06 11:52:15 +0300

    Fixed redirecting to result page

--- src/main/webapp/resources/js/survey/app.js
+++ src/main/webapp/resources/js/survey/app.js
@@ -67,7 +67,7 @@ function SurveyController($rootScope, $scope, $location, SurveyService) {
 
     $scope.submitSurvey = function() {
         SurveyService.submitSurvey($scope.survey).then(function() {
-            location.href = "/result/" + $scope.survey.token;
+            location.href = _contextPath + "/result/" + $scope.survey.token;
         });
     };
 }
